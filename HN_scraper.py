b16877ddf5ce43ecafab53f8ec81a6c8d7fb254c
Unmystic <unmystic7@gmail.com>
2024-09-11 18:44:35 +0200

    created sqlite db for hn articles

--- HN_scraper.py
+++ HN_scraper.py
@@ -2,18 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 import pprint
 
-res = requests.get('https://news.ycombinator.com/news')
-res2 = requests.get('https://news.ycombinator.com/news?p=2')
-soup = BeautifulSoup(res.text, 'html.parser')
-soup2 = BeautifulSoup(res2.text, 'html.parser')
+def get_data():
+    res = requests.get('https://news.ycombinator.com/news')
+    res2 = requests.get('https://news.ycombinator.com/news?p=2')
+    soup = BeautifulSoup(res.text, 'html.parser')
+    soup2 = BeautifulSoup(res2.text, 'html.parser')
 
-links = soup.css.select('.titleline > a')
-subtext = soup.select('.subtext')
-links2 = soup2.css.select('.titleline > a')
-subtext2 = soup2.select('.subtext')
+    links = soup.css.select('.titleline > a')
+    subtext = soup.select('.subtext')
+    links2 = soup2.css.select('.titleline > a')
+    subtext2 = soup2.select('.subtext')
 
-mega_links = links + links2
-mega_subtext = subtext + subtext2
+    mega_links = links + links2
+    mega_subtext = subtext + subtext2
+    return mega_links,mega_subtext
 
 def sort_stories_by_votes(hnlist):
   return sorted(hnlist, key= lambda k:k['votes'], reverse=True)
@@ -29,5 +31,7 @@ def create_custom_hn(links, subtext):
       if points > 99:
         hn.append({'title': title, 'link': href, 'votes': points})
   return sort_stories_by_votes(hn)
- 
-pprint.pprint(create_custom_hn(mega_links, mega_subtext))
+
+if __name__ == "__main__":
+  links, subtext = get_data()
+  pprint.pprint(create_custom_hn(links,subtext))
--- database.py
+++ database.py
@@ -1,7 +1,25 @@
 import sqlite3
+from HN_scraper import create_custom_hn, get_data
+from datetime import date
 
-connection = sqlite3.connect('tasks.db')
+connection = sqlite3.connect('news_articles.db')
 cursor = connection.cursor()
 
 cursor.execute('pragma journal_mode=wal')
-cursor.execute('.mode box')
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS hackernews(id integer primary key not null,
+               title varchar not null,
+               date varchar, 
+               link varchar,
+               votes integer)
+               ''')
+links, subtext = get_data()
+news_list = create_custom_hn(links,subtext)
+for article in news_list:
+    cursor.execute('INSERT OR IGNORE INTO hackernews (title, date, link, votes) VALUES (?, ?, ?, ?)', 
+                   (article['title'], date.today(), article['link'],article['votes']))
+
+
+# Сохраняем изменения и закрываем соединение
+connection.commit()
+connection.close()
